bef00c68c389c3d5ceab26f7b4d548a3dd563463
rye3355 <rye@broadinstitute.org>
2024-04-21 20:09:10 -0400

    Update get-sample-info, confirm gnomadv3 non release had HQ sample filtering

--- subsetting/04_get-sample-info.py
+++ subsetting/04_get-sample-info.py
@@ -19,3 +19,19 @@ meta = meta.filter(hl.is_defined(sample_ht[meta.s]))
 meta = meta.flatten()
 meta = meta.key_by('s')
 meta.export("gs://2024-wgspd/gnomad_v3.1_subset-metadata.tsv", delimiter = "\t")
+
+
+"""
+In our subset, non release does have high quality filtering on it
+
+library(data.table)
+
+gnomad <- fread("../files/gnomad_v3.1_subset-metadata.tsv")
+table(gnomad[, c("high_quality", "release")])
+
+#                   release
+# high_quality FALSE  TRUE
+# FALSE         2788     0
+# TRUE          5174 27565
+
+"""
